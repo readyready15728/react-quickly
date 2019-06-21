e48a09bd61dd64adf7bfcb44611e95e87990c403
Lynn Bradshaw <readyready15728@gmail.com>
2019-06-21 19:37:03 -0400

    Reordering statements in constructor in project clock to make more sense

--- clock/jsx/clock.jsx
+++ clock/jsx/clock.jsx
@@ -1,8 +1,8 @@
 class Clock extends React.Component {
   constructor(props) {
     super(props)
-    this.launchClock();
     this.state = {currentTime: (new Date()).toLocaleString()};
+    this.launchClock();
   }
 
   launchClock() {
