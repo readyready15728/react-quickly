07750f4f7d8e3006890768dcb43bf20a42dce283
Lynn Bradshaw <readyready15728@gmail.com>
2019-06-23 21:21:21 -0400

    Adding content.jsx to project logger

---
+++ logger/jsx/content.jsx
@@ -0,0 +1,27 @@
+class Content extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0,
+      currentTime: (new Date()).toLocaleString()
+    };
+    this.launchClock();
+  }
+
+  launchClock() {
+    setInterval(() => {
+      this.setState({
+        counter: ++this.state.counter,
+        currentTime: (new Date()).toLocaleString()
+      });
+    }, 1000);
+  }
+
+  render() {
+    if (this.state.counter > 2) {
+      return <div/>;
+    }
+
+    return <Logger time={currentTime}/>;
+  }
+}
