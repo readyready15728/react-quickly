efe7b32375b06c8ec5d7b1d1259863a1485476f2
Lynn Bradshaw <readyready15728@gmail.com>
2019-07-11 23:53:51 +0000

    Adding button.jsx to project button

---
+++ button/jsx/button.jsx
@@ -0,0 +1,13 @@
+/* global React */
+
+class SaveButton extends React.Component {
+  handleSave(event) {
+    console.log(this, event);
+  }
+  
+  render() {
+    return <div>
+      <button onClick={this.handleSave.bind(this)}>Save</button>
+    </div>;
+  }
+}
